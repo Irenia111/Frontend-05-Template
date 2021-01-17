4c37f90e9bba5322a8db3db0661674981ad6e1d3
Irenia <15000616693@163.com>
2021-01-17 17:37:40 +0800

    week15

---
+++ Week 15/ui/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: './main.js',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: [['@babel/plugin-transform-react-jsx', {pragma: 'createElement'}]]
+                    }
+                }
+            }
+        ]
+    },
+    devServer: {
+        host: '127.0.0.1',
+        port: '3000',
+        open: true
+    },
+    mode: 'development'
+}
