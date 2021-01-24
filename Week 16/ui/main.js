6875df4aa68806861d2b5c75a66d22e0a3cbf94d
Irenia <15000616693@163.com>
2021-01-24 16:44:40 +0800

    week16

--- Week 16/ui/main.js
+++ Week 16/ui/main.js
@@ -12,5 +12,35 @@ const IMAGES = [
   'https://static001.geekbang.org/resource/image/a6/f1/a691e6b628ceb9d7c2ca9780126301f1.jpg',
   'https://static001.geekbang.org/resource/image/b1/d6/b1b70d207fed37fd54c127f9667d1fd6.jpg'
 ];
-const carousel = <Carousel images={IMAGES} />;
+
+let carouselData = [
+  {
+      img: images[0],
+      url:"https://time.geekbang.org/",
+      title:"1号猫"
+  },{
+      img: images[1],
+      url:"https://time.geekbang.org/",
+      title:"2号猫"
+  },{
+      img: images[2],
+      url:"https://time.geekbang.org/",
+      title:"3号猫"
+  },{
+      img: images[3],
+      url:"https://time.geekbang.org/",
+      title:"4号猫"
+  }
+]
+
+// const carousel = <Carousel images={IMAGES} />;
+// carousel.mountTo(document.body);
+let carousel = <List data={carouselData}>
+    {(record) =>
+        <div>
+            <img src={record.img} />
+            <a href={record.url}>{record.title}</a>
+        </div>
+    }
+</List>
 carousel.mountTo(document.body);
